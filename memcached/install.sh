6a056cb7548e8da53e93d06f6fc669f8ffcc4267
andychen <bootoo@sina.cn>
2017-10-01 20:41:10 +0800

    添加memcached

---
+++ memcached/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#wget http://memcached.org/files/memcached-1.5.2.tar.gz
+#rm -fr ./memcached-1.5.2
+#tar -zxvf ./memcached-1.5.2.tar.gz
+#cd ./memcached-1.5.2
+#./configure --prefix=/usr/local/memcached
+#make && make install
+
+
+#安装libmemcached
+#wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz --no-check-certificate
+#tar -zxvf ./libmemcached-1.0.18.tar.gz
+#cd ./libmemcached-1.0.18
+#./configure --prefix=/usr/local/libmemcached  --with-memcached
+#make && make install
+
+#安装php memcached扩展
+#wget https://pecl.php.net/get/memcached-2.2.0.tgz --no-check-certificate
+#tar -zxvf ./memcached-2.2.0.tgz
+#cd ./memcached-2.2.0
+#phpize
+#./configure --enable-memcached --with-php-config=/andydata/server/php/bin/php-config --with-libmemcached-dir=/usr/local/libmemcached
+#./configure --with-memcached
+#make && make install
+
+
+
+
+
